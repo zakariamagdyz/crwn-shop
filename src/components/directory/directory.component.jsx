5cc219854972ca0696d193b6d80e0597f7545ab4
zakariamagdyz git config --global user.name zakariamagdyz <zakariamagdy@ymail.com>
2021-12-16 13:56:51 +0200

    broken our hompage into smaller components, finished our menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { sections } from "../../assets/072 directory.data";
+import "./directory.styles.scss";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sections };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map((section) => (
+          <MenuItem key={section.id} {...section} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
